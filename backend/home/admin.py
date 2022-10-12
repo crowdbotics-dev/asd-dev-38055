354a7f7d1fba8c4ea4ed9366b2c5ac65bba2a3c1
Crowdbotics <engineering@crowdbotics.com>
2022-10-12 12:29:56 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Nvbcndc
+
+admin.site.register(Nvbcndc)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Nvbcndc(models.Model):
+    "Generated Model"
+    nmm = models.BigIntegerField()
