a21050bcf1008cab7987ecf9978e7852c9aa75eb
Crowdbotics <engineering@crowdbotics.com>
2022-10-18 06:07:48 +0000

    Update admin title

--- backend/asd_dev_38055/urls.py
+++ backend/asd_dev_38055/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Asd"
-admin.site.site_title = "Asd Admin Portal"
-admin.site.index_title = "Asd Admin"
+admin.site.site_header = "Asd123"
+admin.site.site_title = "Asd123 Admin Portal"
+admin.site.index_title = "Asd123 Admin"
 
 # swagger
 api_info = openapi.Info(
